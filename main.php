6c3c45578e4a47a31fdce995c6d738d341ccf6ce
nathyelle atahides cavanos <nathyelle.cavanos@escola.pr.gov.br>
2026-06-30 11:39:03 +0000

    .

--- main.php
+++ main.php
@@ -1,28 +1,64 @@
 <?php
-class tabela{
-    public $nome;
-    public $pdo;
-  public function __construct($nome, $host, $port, $dbname, $user, $password){
-    $this->nome = $nome;
-    try{
-      $this->pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
-      $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      echo "Conexão realizada com sucesso!";
-    }catch(PDOException $e){
-    die("Erro na conexão:" . $e->getMessage() . "\n");
+//  class circulo{
+//      public $radio;
+//      public function __construct($radio){
+//          $this->radio = $radio;
+//      }
+//      public function calcularArea(){
+//          return 3.14 * $this->radio * $this->radio;
+      
+//      }
+//      public function calcularCircunferencia(){
+//          return 2 * 3.14 * $this->radio;
+//      }
+//  }
+// $circulo = new circulo(8);
+// echo $circulo->calcularArea() . "\n";
+// echo $circulo->calcularCircunferencia() . "\n";
 
-  }
-  }
-  public function listarAlunos(){
-    try{
-    $sql = "SELECT * FROM $this->nome";
-    $stmt = $this->pdo->query($sql);
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-   }catch(PDOException $e){
-      die("Erro na conexão:" . $e->getMessage() . "\n");
+class Contabancaria {
+    public $numeroConta;
+    public $nomeTitular;
+    public $saldodaconta;
+
+    public function __construct($numeroConta, $nomeTitular, $saldodaconta = 0) {
+        $this->numeroConta = $numeroConta;
+        $this->nomeTitular = $nomeTitular;
+        $this->saldodaconta = $saldodaconta;
     }
-   }
+
+    public function depositarvalor($valor) {
+        if ($valor > 0) {
+            $this->saldodaconta += $valor;
+            return "Depósito realizado com sucesso.";
+        } else {
+            return "Não é possível depositar um valor negativo ou zero.";
+        }
+    }
+
+    public function sacarvalor($valor) {
+        if ($valor <= 0) {
+            return "Não é possível sacar um valor negativo ou zero.";
+        }
+
+        if ($valor > $this->saldodaconta) {
+            return "Saldo insuficiente.";
+        }
+
+        $this->saldodaconta -= $valor;
+        return "Saque realizado com sucesso.";
+    }
+
+    public function consultarsaldo() {
+        return $this->saldodaconta;
+    }
 }
-$alunos = new tabela("alunos","aws-1-sa-east-1.pooler.supabase.com", "6543", "postgres", "postgres.kwebdjvwxeczoxlxzckk","Ec827X0sLCVH4kiR");
-print_r($alunos->listarAlunos());
+
+$ana = new Contabancaria("00000-A", "Ana", 6000);
+echo $ana->sacarvalor(650) . "\n";
+echo $ana->depositarvalor(1000) . "\n";
+echo "Saldo atual: R$ " . $ana->consultarsaldo() . "\n";
+echo $ana->depositarvalor(-100) . "\n";
+echo $ana->sacarvalor(100000) . "\n";
+
 ?>
